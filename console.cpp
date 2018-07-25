83f6735a15a7b471442ea891550d92f853134745
Mark <vipdos007@gmail.com>
2018-07-25 13:43:21 +0800

    Fix bug.

--- console.cpp
+++ console.cpp
@@ -15,49 +15,49 @@ void Console::print(char* message, std::ostream& color)
     std::cout << color << message << std::endl;
 }
 
-std::ostream& Color::grey(std::ostream& stream);
+std::ostream& Color::grey(std::ostream& stream)
 {
     stream << "\033[30m";
     return stream;
 }
 
-std::ostream& Color::red(std::ostream& stream);
+std::ostream& Color::red(std::ostream& stream)
 {
     stream << "\033[31m";
     return stream;
 }
 
-std::ostream& Color::green(std::ostream& stream);
+std::ostream& Color::green(std::ostream& stream)
 {
     stream << "\033[32m";
     return stream;
 }
 
-std::ostream& Color::yellow(std::ostream& stream);
+std::ostream& Color::yellow(std::ostream& stream)
 {
     stream << "\033[33m";
     return stream;
 }
 
-std::ostream& Color::blue(std::ostream& stream);
+std::ostream& Color::blue(std::ostream& stream)
 {
     stream << "\033[34m";
     return stream;
 }
 
-std::ostream& Color::magenta(std::ostream& stream);
+std::ostream& Color::magenta(std::ostream& stream)
 {
     stream << "\033[35m";
     return stream;
 }
 
-std::ostream& Color::cyan(std::ostream& stream);
+std::ostream& Color::cyan(std::ostream& stream)
 {
     stream << "\033[36m";
     return stream;
 }
 
-std::ostream& Color::white(std::ostream& stream);
+std::ostream& Color::white(std::ostream& stream)
 {
     stream << "\037[37m";
     return stream;
